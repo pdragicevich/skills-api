90f2f6a12fda4096a885192c3a8887ed848bd2d8
Paul Dragicevich <pdragicevich@gmail.com>
2021-12-04 06:42:03 +0800

    Fix launch settings

--- SkillsAPI.Albemuth.Net/Controllers/SkillsController.cs
+++ SkillsAPI.Albemuth.Net/Controllers/SkillsController.cs
@@ -5,42 +5,42 @@ using SkillsAPI.Albemuth.Net.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace SkillsAPI.Albemuth.Net.Controllers
+namespace SkillsAPI.Albemuth.Net.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SkillsController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class SkillsController : ControllerBase
+    private readonly ILogger<SkillsController> logger;
+    private readonly ISkillsRepo skillsRepo;
+
+    public SkillsController(
+        ILogger<SkillsController> logger,
+        ISkillsRepo skillsRepo
+        )
     {
-        private readonly ILogger<SkillsController> logger;
-        private readonly ISkillsRepo skillsRepo;
+        this.logger = logger;
+        this.skillsRepo = skillsRepo;
+    }
 
-        public SkillsController(
-            ILogger<SkillsController> logger,
-            ISkillsRepo skillsRepo
-            )
-        {
-            this.logger = logger;
-            this.skillsRepo = skillsRepo;
-        }
+    [HttpGet]
+    [Route("")]
+    public async Task<IEnumerable<SkillSummary>> GetSummaryList()
+    {
+        return await skillsRepo.GetSkillSummaries();
+    }
 
-        [HttpGet]
-        [Route("")]
-        public async Task<IEnumerable<SkillSummary>> GetSummaryList()
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<ActionResult<SkillDetail>> GetDetail(string id)
+    {
+        var detail = await skillsRepo.GetSkillByID(id);
+        if (detail == null)
         {
-            return await skillsRepo.GetSkillSummaries();
+            logger.LogWarning($"GetDetail 404 for {id}");
+            return NotFound();
         }
-
-        [HttpGet]
-        [Route("{id}")]
-        public async Task<ActionResult<SkillDetail>> GetDetail(string id)
-        {
-            var detail = await skillsRepo.GetSkillByID(id);
-            if (detail == null)
-            {
-                logger.LogWarning($"GetDetail 404 for {id}");
-                return NotFound();
-            }
-            return Ok(detail);
-        }
+        return Ok(detail);
     }
 }
+
